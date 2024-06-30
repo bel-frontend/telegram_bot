c0f0765f32e204c1d4f8cbb69a88718127b19763
bel_frontend <goman.live.service@gmail.com>
2024-06-30 22:11:26 +0200

    update  bot

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,5 @@
 
 docker build -t telegram_bot  .
 docker stop telegram_bot || true && docker rm telegram_bot || true
-<<<<<<< HEAD
-docker run -d -p 3004:3001 --name telegram_bot --restart unless-stopped telegram_bot
-=======
 docker run -d -p 3004:3000 --name telegram_bot --restart unless-stopped telegram_bot
->>>>>>> main
+
