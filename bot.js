8214ab6be1ea1e2e53fca20b55ff85a083859e54
bel_frontend <goman.live.service@gmail.com>
2024-07-11 13:08:19 +0200

    added json

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
 const TelegramBot = require("node-telegram-bot-api");
+const json = require("./index.json");
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 const TARGET_CHAT_ID = process.env.TARGET_CHAT_ID; // Add your target chat ID in the .env file
 const TARGET_NOTIFICATION_ID = process.env.TARGET_NOTIFICATION_ID; // Add your target chat ID in the .env file
+//  get random message from 1  to 770
+const randomMessage = json[Math.floor(Math.random() * json.length)];
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
@@ -18,6 +21,14 @@ bot.on("message", (msg) => {
       bot
         .deleteMessage(chatId, msg.message_id)
         .then(() => {
+          //           const randomMessage = json[Math.floor(Math.random() * json.length)];
+          //           sendMessage(
+          //             TARGET_CHAT_ID,
+          //             `Тут было паведамленне якое не суадносіцца з нашай суполкай. Мы яго выдалілі і замест  гэтага  трымайце беларускую  прыказку ці прымаўку:
+
+          // "${randomMessage.message}"
+          //              `
+          //           );
           sendMessage(
             TARGET_NOTIFICATION_ID,
             `Message deleted: ${text} from author: ${msg.from.username}`
